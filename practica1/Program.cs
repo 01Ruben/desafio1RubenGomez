81a33c1aaebca5181ec287cadd1bbc986f9d4c41
Ruben <destego@gmail.com>
2023-12-14 22:59:05 -0300

    creacion repositorio

---
+++ practica1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+namespace practica1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "Ruben";
+            string apellido = "Gomez";
+            int dni = 25264164;
+            int edad = 47;
+            float altura = 1.68f;
+
+            Console.WriteLine("mi nombre es " + nombre +" "+ apellido + " con dni " + dni + " tengo " + edad + " años y mi altura es " + altura);
+
+
+            int numero1 = 25;
+            int numero2 = 30;
+
+            Console.WriteLine("La suma de los dos numeros es "+numero1 + numero2);
+            Console.WriteLine("mi nombre es "+ nombre + " Destego " + apellido);
+            Console.ReadKey();
+        }
+    }
+}
